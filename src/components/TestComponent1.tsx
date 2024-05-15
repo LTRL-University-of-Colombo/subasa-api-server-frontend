3f037f0d098321fc5b46ccb42ae696519525bf47
anjuna0305 <anjunaserasingha@gmail.com>
2024-05-15 23:46:32 +0530

    sass files added

---
+++ src/components/TestComponent1.tsx
@@ -0,0 +1,11 @@
+const TestComponent1 = () => {
+    return (
+        <>
+            <button className={"btn btn-primary btn-sm"}>this is a button</button>
+            <button className={"btn btn-secondary btn-sm"}>this is a button</button>
+            <button className={"btn btn-custom119 btn-sm"}>this is a button</button>
+        </>
+    )
+}
+
+export default TestComponent1
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './assets/css/custom_bootstrap.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import TestComponent1 from "./components/TestComponent1.tsx";
 
 const router = createBrowserRouter([{
     path: "/",
-    element: <div>This is root</div>
+    element: <TestComponent1/>
 }])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
