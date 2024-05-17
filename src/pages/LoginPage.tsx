57b058af81acc41189c3342b4ea7f7240b0d92ab
ltrlpc1 <ltrlpc1@proton.me>
2024-05-17 16:14:52 +0530

    login page done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,28 @@
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <>
+            <div className="row vw-100 vh-100">
+                <div className="col d-flex justify-content-center align-items-center">
+                    <h1>Login to SUBASA</h1>
+                </div>
+                <div className="col d-flex justify-content-center align-items-center bg-secondary">
+                    <form className="card p-4" style={{ width: "300px" }}>
+                    <h2>Login</h2>
+                        <div className="mb-3 mt-3">
+                            <label className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LoginPage
